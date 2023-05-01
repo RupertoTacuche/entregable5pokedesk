9e952b64d9d46e9c38b50ef3755539d6740c9f5b
RupertoTacuche <murcio2490@gmail.com>
2023-05-01 17:39:55 -0600

    pokemonesYaseven

---
+++ src/components/pokedex/PokemonCard.jsx
@@ -0,0 +1,61 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+
+const bordersByType = {
+    grass: "border-green-500",
+    fire: "border-red-500"
+}
+
+const backgroundByType = {
+    grass: "from-green-500 to-black",
+    fire: "from-red-500 to-black"
+
+}
+
+const PokemonCard = ({pokemonUrl}) => {
+
+    const [pokemon, setPokemon] = useState()
+
+    const types = pokemon?.types.slice(0,2).map(type => type.type.name).join(" / ")
+
+    useEffect(() => {
+        axios.get(pokemonUrl)
+            .then((res) => setPokemon(res.data))
+            .catch((err) => console.log(err))
+    },[])
+
+  return (
+     
+    <article className={`text-center border-8 rounded-md ${bordersByType[pokemon?.types[0].type.name]}`}>
+        {/* Seccion superior */}
+        <section className={`bg-gradient-to-b ${backgroundByType[pokemon?.types[0].type.name]} relative h-[150px]`}>
+            <div className="absolute -bottom-12 w-[200px] left-1/2 -translate-x-1/2">
+                <img src={pokemon?.sprites.other["official-artwork"].front_default} alt="" />
+            </div>
+
+        </section>
+        {/* Seccion inferior */}
+        <section>
+            <h3 className="mt-10">{pokemon?.name}</h3>
+            <h4>{types}</h4>
+            <span>Type</span>
+
+            <hr />
+            <section className="grid grid-cols-3 gap-2 p-2">
+                {
+                    pokemon?.stats.map(stat => (
+                        <div key={stat.stat.name}>
+                            <h5>{stat.stat.name}</h5>
+                            <span>{stat.base_stat}</span>
+                        </div>
+                    ))
+                }
+            </section>
+
+        </section>
+
+    </article>
+  )
+}
+
+export default PokemonCard
--- src/pages/Pokedex.jsx
+++ src/pages/Pokedex.jsx
@@ -1,36 +1,55 @@
 import { useSelector } from "react-redux"
 import Header from "../components/pokedex/Header"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import axios from "axios"
+import PokemonCard from "../components/pokedex/PokemonCard"
 
 const Pokedex = () => {
 
+  const [pokemons, setPokemons] = useState([])
+  const [pokemonName, setPokemonName] = useState("")
+  console.log(pokemonName)
+
   const nameTrainer = useSelector(store => store.nameTrainer)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setPokemonName(e.target.pokemonName.value)
+
+  }
 
   useEffect(() => {
     const URL="https://pokeapi.co/api/v2/pokemon"
 
     axios.get(URL)
-      .then((res) => console.log(res.data))
+      .then((res) => setPokemons(res.data.results))
       .catch((err) => console.log(err))
   },[])
 
   return (
     <section className="min-h-screen">
       <Header />
+      {/* Seccion de filtros y saludo */}
       <section className="py-6 px-2">
         <h3>Welcome {nameTrainer}, here you can find your favorite pokemon</h3>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
-            <input type="text" placeholder="Search your pokemon"/>
-            <buttom>Search</buttom>
+            <input id="pokemonName" type="text" placeholder="Search your pokemon" />
+              <button>Search</button>
+            
           </div>
 
           <select>
             <option value="">All</option>
           </select>
         </form>
+      </section>
+      {/* seccion lista de pokemons*/}
+      <section className="px-2 grid gap-6 grid-cols-[280px]">
+        {
+          pokemons.map(pokemon => <PokemonCard key={pokemon.url} pokemonUrl={pokemon.url}/>)
+        }
       </section>
     </section>
   )
