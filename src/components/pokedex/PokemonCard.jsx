9ac1f860eb6b5bde726122772fdaab47c544fd38
RupertoTacuche <murcio2490@gmail.com>
2023-05-02 15:26:27 -0600

    errorBarraStats

--- src/components/pokedex/PokemonCard.jsx
+++ src/components/pokedex/PokemonCard.jsx
@@ -1,5 +1,6 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
 
 const bordersByType = {
     grass: "border-green-500",
--- src/pages/PokemonId.jsx
+++ src/pages/PokemonId.jsx
@@ -1,6 +1,47 @@
+import { useEffect, useState } from "react"
+import Header from "../components/pokedex/Header"
+import { useParams } from "react-router-dom"
+import axios from "axios"
+
 const PokemonId = () => {
+  const [pokemon, setPokemon] = useState()
+  console.log(pokemon)
+  const {id} = useParams()
+  
+  useEffect(() => {
+    const URL =`https://pokeapi.co/api/v2/pokemon/${id}/`
+    axios.get(URL)
+      .then((res) => setPokemon(res.data))
+      .catch((err) => console.log(err))
+  },[])
+
   return (
-    <div>PokemonId</div>
+    <section>
+      <Header />
+
+      <section className="px-2 py-10">
+        <article>
+          {/* Todo lo demas */}
+          {/* stats */}
+          <section>
+            <h3>Stats</h3>
+            <section>
+              {
+                pokemon.stats.map(stat => (
+                  <article>
+                    <section>
+                      <h5>{stat.stat.name}</h5>
+                      <span>{stat.base_stat}/150</span>
+                    </section>
+                  </article>
+
+                ))
+              }
+            </section>
+          </section>
+        </article>
+      </section>
+    </section>
   )
 }
 
