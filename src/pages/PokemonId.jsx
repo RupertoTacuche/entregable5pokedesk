e21ca7121dbb5e03f2913ec39e70fd190a8cfcfd
RupertoTacuche <murcio2490@gmail.com>
2023-05-03 05:15:21 -0600

    YaTercerVideo

--- src/pages/PokemonId.jsx
+++ src/pages/PokemonId.jsx
@@ -24,8 +24,39 @@ const getPercentStatbar = (stat_base) => {
 
       <section className="px-2 py-10">
         <article>
-          {/* Todo lo demas */}
-          {/* stats */}
+          {/* Seccion superior */}
+          <section>
+            <div>
+              <img src={pokemon?.sprites.other["official-artwork"].front_default} alt="" />
+            </div>
+          </section>
+
+          {/* Informacion general*/}
+          <section>
+            <div>
+              <h3>#{pokemon?.id}</h3>
+            </div>
+            <div className="grid grid-cols-[1fr_auto_1fr] items-center gap-2">
+              <hr />
+              <h2 className="capitalize font-bold" >{pokemon?.name}</h2>
+              <hr />
+            </div>
+
+            <div className="flex justify-center gap-6 text-center">
+              <div>
+                <h5>Weight</h5>
+                <span>{pokemon?.weight}</span>
+
+              </div>
+              <div>
+                <h5>Height</h5>
+                <span>{pokemon?.height}</span>
+
+              </div>
+            </div>
+
+          </section>
+          {/* seccion de stats */}
           <section>
             <h3>Stats</h3>
             <section>
@@ -34,7 +65,7 @@ const getPercentStatbar = (stat_base) => {
                   <article key={stat.stat.name}>
                     <section className="flex justify-between">
                       <h5 className="capitalize">{stat.stat.name}</h5>
-                      <span>{stat.base_stat}/150</span>
+                      <span>{stat.base_stat}/255</span>
                     </section>
                     <div className=" bg-gray-100 h-6 rounded-sm">
                       <div style={{"width": getPercentStatbar(stat.base_stat)}} className={`h-full  bg-gradient-to-r  from-yellow-300 to-yellow-600`}></div>
